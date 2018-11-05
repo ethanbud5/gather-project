2b156b37f39c9da2d2abbcdbab9ffabef8b35209
Ethan Sanders <ethanedu5@gmail.com>
2018-11-05 11:27:14 -0600

    Can now click on names and zoom to point on map

--- src/Views/Map/Map.js
+++ src/Views/Map/Map.js
@@ -1,53 +1,128 @@
 import React, { Component } from 'react';
 import SubNavbar from "./../../Components/SubNavbar/SubNavbar";
 import { withGoogleMap, GoogleMap, Marker, InfoWindow } from 'react-google-maps';
+import axios from "axios";
+import "./Map.css"
+
 const { LatLng, LatLngBounds } = window.google.maps;
 
-
 class Map extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            profiles:[],
+            profileActive:null,
+            activeCords:{
+                lat:null,
+                lng:null
+            }
+        }
+        this.selectProfile = this.selectProfile.bind(this);
+        this.resetMap = this.resetMap.bind(this);
+    }
+    
+    componentDidMount(){
+        axios.get("/api/profiles/"+this.props.match.params.id).then(res=>{
+            console.log(res.data);
+            this.setState({
+                profiles: res.data.profiles
+            })
+        }).catch(err=>console.log(err))
+    }
     fitBounds(map){
         if (!map) return;
         const bounds = new LatLngBounds();
-            const coords1 = new LatLng(parseFloat(33.0446168), parseFloat(-86.1563089));
-            bounds.extend(coords1);
-            const coords2 = new LatLng(parseFloat(40.756795), parseFloat(-73.954298));
-            bounds.extend(coords2);
+        this.state.profiles.map(profile=>{
+            const coords = new LatLng(parseFloat(profile.lat), parseFloat(profile.lng));
+            bounds.extend(coords);
+
+        })
     // console.log(map)
 
         map.fitBounds(bounds);
     }
+    selectProfile(id,coords){
+        this.setState({
+            profileActive:id,
+            activeCords:{
+                lat:coords.lat,
+                lng:coords.lng
+            }
+        })
+    }
+    resetMap(){
+        this.setState({
+            profileActive: null,
+            activeCords:{
+                lat:null,
+                lng:null
+            }
+        })
+    }
     render() {
-        // console.log(window.google.maps)
-        const GoogleMapExample = withGoogleMap(props => (
-            <GoogleMap
-              defaultCenter = { { lat: 40.756795, lng: -86.2196937 } }
-              defaultZoom = { 13 }
-              ref={(map)=>this.fitBounds(map)}
-              
-              
-            //   ref={(map) => { console.log(map.fitBounds({ lat: -34.397, lng: 150.644 })) }}
+        console.log(this.state)
+        let markers = this.state.profiles.map(profile=>(
+            <Marker 
+               position={{ lat: +profile.lat, lng: +profile.lng }}
+               onClick={()=>console.log({ lat: +profile.lat, lng: +profile.lng })}
+               key={profile.profile_id}
+           />
+        ))
+        const GoogleMapExample = withGoogleMap(props => {
+            return (this.state.profileActive)?
+             <GoogleMap
+              defaultCenter = { { lat: +this.state.activeCords.lat, lng: +this.state.activeCords.lng } }
+              defaultZoom = { 20 }
+            //   ref={(map)=>this.fitBounds(map)}
             >
-             <Marker 
-                position={{ lat: 33.0446168, lng: -86.1563089 }}
-                onClick={()=>console.log({ lat: 33.0446168, lng: -86.1563089 })}
-            />
-             <Marker 
+            {markers}
+             {/* <Marker 
                 position={{ lat: 40.756795, lng: -73.954298 }}
             >
                 <InfoWindow>
                     <h1>Hey</h1>
                 </InfoWindow>
-            </Marker>
+            </Marker> */}
             </GoogleMap>
-         ));
+            
+            :
+             <GoogleMap
+              defaultCenter = { { lat: 40.756795, lng: -86.2196937 } }
+              defaultZoom = { 13 }
+              ref={(map)=>this.fitBounds(map)}
+            >
+            {markers}
+             {/* <Marker 
+                position={{ lat: 40.756795, lng: -73.954298 }}
+            >
+                <InfoWindow>
+                    <h1>Hey</h1>
+                </InfoWindow>
+            </Marker> */}
+            </GoogleMap>
+        
+         });
+         let sideInfo = this.state.profiles.map(profile=>(
+             <div key={profile.profile_id} className={(this.state.profileActive === profile.profile_id)?"profile_card_map active_profile_map":"profile_card_map"} onClick={()=>this.selectProfile(profile.profile_id,{lat:profile.lat,lng:profile.lng})}>
+                <h1>{profile.name}</h1>
+                <p>{profile.address}</p>
+             </div>
+         ))
         return (
             <div>
                 <SubNavbar path="/map" id={this.props.match.params.id} history={this.props.history}/>
-                <h1>Map</h1>
-                <GoogleMapExample
-                    containerElement={ <div style={{ height: `500px`, width: '100%' }} /> }
-                    mapElement={ <div style={{ height: `100%` }} /> }
-                />
+                <div className="map_container">
+                    <div className="left_side_map">
+                    <button onClick={this.resetMap}>Reset Map</button>
+                        {sideInfo}
+                    </div>
+                    <div className="right_side_map">
+                        <GoogleMapExample
+                            containerElement={ <div style={{ height: `88vh`, width: '100%' }} /> }
+                            mapElement={ <div style={{ height: `100%` }} /> }
+                        />
+                    </div>
+                </div>
             </div>
         );
     }
