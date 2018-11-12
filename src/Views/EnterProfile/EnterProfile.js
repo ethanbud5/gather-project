8d21edd16ae6db840abbdcbd474ec850a1ec43a4
Ethan Sanders <ethanedu5@gmail.com>
2018-11-11 23:02:01 -0600

    added Get Address feature to enter profile

--- src/Views/EnterProfile/EnterProfile.js
+++ src/Views/EnterProfile/EnterProfile.js
@@ -3,6 +3,7 @@ import Navbar from '../../Components/Navbar/Navbar';
 import "./EnterProfile.css";
 import Axios from 'axios';
 import Geocode from "react-geocode";
+import Modal from "react-modal";
 
 
 class EnterProfile extends Component {
@@ -23,11 +24,16 @@ class EnterProfile extends Component {
             custom1Name:"Custom 1",
             custom2Name:"Custom 2",
             custom3Name:"Custom 3",
-            statusMessage:""
+            statusMessage:"",
+            showModal:false,
+            currentAddresses:[]
 
         }
         this.changeHandler = this.changeHandler.bind(this);
         this.addNewProfile = this.addNewProfile.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
+        this.getCurrentAddress = this.getCurrentAddress.bind(this);
+        this.selectAddress = this.selectAddress.bind(this);
     }
     componentDidMount(){
         //TODO: if i use this view for the users, then i need to make a conditional 
@@ -119,8 +125,58 @@ class EnterProfile extends Component {
         );
 
     }
+    getCurrentAddress(){
+        navigator.geolocation.getCurrentPosition(
+            ( position )=>{ // success cb
+ 
+                /* Current Coordinate */
+                var lat = position.coords.latitude;
+                var lng = position.coords.longitude;
+                Geocode.setApiKey(process.env.REACT_APP_GOOGLE_MAPS_API_KEY);
+                Geocode.fromLatLng(lat, lng).then(
+                    response => {
+                      const address = response
+                      console.log(address);
+                      this.setState({
+                          currentAddresses:response.results.filter(address=>address.address_components.length>6),
+                          showModal:true
+                        })
+                    },
+                    error => {
+                      console.error(error);
+                    }
+                  );
+            },
+            ()=>{ // fail cb
+            }
+        );
+    }
+    toggleModal(){
+        this.setState({showModal:!this.state.showModal})
+    }
+    selectAddress(address){
+        // console.log(address)
+        if(address.length ===9 || address.length ===8){
+            this.setState({
+                address:address[0].short_name+" "+address[1].short_name,
+                city:address[3].short_name,
+                state:address[5].short_name,
+                zip:address[7].short_name,
+                showModal:false
+            })
+        }
+        if(address.length===7){
+            this.setState({
+                address:address[0].short_name,
+                city:address[2].short_name,
+                state:address[4].short_name,
+                zip:address[6].short_name,
+                showModal:false
+            })
+        }
+    }
     render() {
-        // console.log(this.state)
+        console.log(this.state)
         let {
             name,
             phone,
@@ -133,9 +189,27 @@ class EnterProfile extends Component {
             custom2,
             notes
         } = this.state
+        let listAddressOptions = this.state.currentAddresses.map((address,i)=>{
+            return(
+                <div className="address_card" key={i} onClick={()=>this.selectAddress(address.address_components)}>
+                    <h1>{address.formatted_address}</h1>
+                </div>
+            )
+        })
         return (
             //TODO: make sure that the values being inputed are valid
+            
             <div>
+               <Modal 
+                isOpen={this.state.showModal}
+                ariaHideApp={false}
+                onRequestClose={this.toggleModal}
+                
+                >
+                    <div className="current_addresses">
+                    {listAddressOptions}
+                    </div>
+                </Modal>
                 <Navbar path="/enter-profile" history={this.props.history}/>
                     <div className="profile_form_container">
                         <div className="inner_flex_inputs">
@@ -152,6 +226,7 @@ class EnterProfile extends Component {
                         </div>
                         <div className="inner_flex_inputs">
                             <label>Address*</label>
+                            <button className="get_address_btn" onClick={this.getCurrentAddress}>Get Address</button>
                             <input className="input_wide" value={address} name="address" type="text" onChange={this.changeHandler}/>
                         </div>
                         <div className="inner_flex_inputs">
@@ -164,7 +239,7 @@ class EnterProfile extends Component {
                         </div>
                         <div className="inner_flex_inputs">
                             <label>Zip*</label>
-                            <input maxlength="5" className="input_wide" value={zip} name="zip" type="number" onChange={this.changeHandler}/>
+                            <input className="input_wide" value={zip} name="zip" type="number" onChange={this.changeHandler}/>
                         </div>
                         <div className="inner_flex_inputs">
                             <label>{this.state.custom1Name}</label>
